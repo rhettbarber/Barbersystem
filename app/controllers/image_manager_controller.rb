5e9765cee3a047751fa3e1f6aa366f76cf5f09b1
Rhett Barber <b_rhettbarber@yahoo.com>
2014-12-10 18:16:28 -0500

    Merge branch 'develop'
    Conflicts:
    	config/routes.rb

--- app/controllers/image_manager_controller.rb
+++ app/controllers/image_manager_controller.rb
@@ -1,20 +1,79 @@
 class ImageManagerController < ApplicationController
-before_filter :redirect_unless_admin
+  before_filter :redirect_unless_admin
 
-  def update_missing_images
-    MissingImages.destroy_all
+  before_filter  :initialize_variables
+  before_filter :no_item_menu
 
-  end
+
+ # changed category class id of all in department 35 from 31 to 69
+
 
 
 
     def index
-      @items = ItemsWithYearlySale.find(:all,:conditions => ["sale_year = ? and department_id in (?)", Date.current.year   ,  @website.default_item_department_ids.split(/,/) ], :limit => 200, :order => "date_difference DESC"  )
-#  @items = ItemsWithYearlySales.find(:all,:conditions => ["sale_year = ? and department_id in (?)", Date.current.year   ,  @website.default_item_department_ids.split(/,/) ], :order => "date_difference DESC"  )
-
+                @sublimation_entries = SublimationEntry.group("purchase_id, FirstName,LastName, Company").select("purchase_id, FirstName,LastName, Company").all        #order("purchase_id")
+      s =  "s"
     end
 
-    
+
+
+  def order_details
+            @sublimation_entries = SublimationEntry.where( "purchase_id = ?",  params[:purchase_id] )
+            @sublimation_entry =   @sublimation_entries.all.first
+  end
+
+
+
+  def print_all_over_ts
+                          # system( 'convert W:\AUTOMATION_DATABASE\AUTOMATED_ITEM_SPECSHEETS\\'  + oj.filename + '  -geometry 75 W:\AUTOMATION_DATABASE\FINISHED_WEBSITE_ITEM_THUMBNAILS\\' +  oj.filename )
+                          # montage   -border 0 -geometry 7600x  -density 200 * final.jpg
+                        logger.debug "5555555555555555555555555555555555"
+                        logger.debug "5555555555555555555555555555555555"
+                        logger.debug "5555555555555555555555555555555555"
+                        @sublimation_entries = SublimationEntry.where( "purchase_id = ? and name = ?",  params[:purchase_id], "all_over_item" )
+                        @montage_strings = Set.new
+                        @sublimation_entries.each do |pe|
+                                    # logger.debug "pe.id: " + pe.id.to_s
+
+                                   @montage_string_front  = "montage  -border 0 -geometry 7600x  -density 200 " + pe.file_url_front + " " +  pe.hot_folder_url_front
+                                    @montage_strings.add(@montage_string_front)
+                                   # logger.debug "montage_string_front: " + @montage_string_front
+
+                                   @montage_string_back  = "montage  -border 0 -geometry 7600x  -density 200 " + pe.file_url_back + " " +  pe.hot_folder_url_back
+                                    @montage_strings.add(@montage_string_back)
+                                   # logger.debug "montage_string_back: " + @montage_string_back
+
+                        end
+                        logger.debug "5555555555555555555555555555555555"
+                        logger.debug "5555555555555555555555555555555555"
+                        logger.debug "5555555555555555555555555555555555"
+  end
+
+
+  def print_big_fronts
+                # system( 'convert W:\AUTOMATION_DATABASE\AUTOMATED_ITEM_SPECSHEETS\\'  + oj.filename + '  -geometry 75 W:\AUTOMATION_DATABASE\FINISHED_WEBSITE_ITEM_THUMBNAILS\\' +  oj.filename )
+                # montage   -border 0 -geometry 7600x  -density 200 * final.jpg
+                logger.debug "5555555555555555555555555555555555"
+                logger.debug "5555555555555555555555555555555555"
+                logger.debug "5555555555555555555555555555555555"
+                @sublimation_entries = SublimationEntry.where( "purchase_id = ? and name = ?",  params[:purchase_id], "huge_front" )
+                @montage_strings = Set.new
+                @sublimation_entries.each do |pe|
+                  # logger.debug "pe.id: " + pe.id.to_s
+
+                  @montage_string_front  = "montage  -border 0 -geometry 3800x  -rotate 90 -density 200 " + pe.file_url_front + " " +  pe.hot_folder_url_front
+                  @montage_strings.add(@montage_string_front)
+                  # logger.debug "montage_string_front: " + @montage_string_front
+
+                  @montage_string_back  = "montage  -border 0 -geometry 3800x  -rotate 90  -density 200 " + pe.file_url_back + " " +  pe.hot_folder_url_back
+                  @montage_strings.add(@montage_string_back)
+                  # logger.debug "montage_string_back: " + @montage_string_back
+
+                end
+                logger.debug "5555555555555555555555555555555555"
+                logger.debug "5555555555555555555555555555555555"
+                logger.debug "5555555555555555555555555555555555"
+  end
 
 
 
@@ -22,25 +81,17 @@ before_filter :redirect_unless_admin
 
 
 
+  def update_missing_images
+  MissingImages.destroy_all
+
+end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#     def index
+#       @items = ItemsWithYearlySale.find(:all,:conditions => ["sale_year = ? and department_id in (?)", Date.current.year   ,  @website.default_item_department_ids.split(/,/) ], :limit => 200, :order => "date_difference DESC"  )
+# #  @items = ItemsWithYearlySales.find(:all,:conditions => ["sale_year = ? and department_id in (?)", Date.current.year   ,  @website.default_item_department_ids.split(/,/) ], :order => "date_difference DESC"  )
+#
+#     end
 
 
 
