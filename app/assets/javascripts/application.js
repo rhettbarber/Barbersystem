362ec36f5b8cc02df1260239a2ad310ec849f977
cdr-data <crazygorillabear@gmail.com>
2015-01-30 16:19:52 -0500

    -latest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 
+//= require remove_console
 //= require jquery-1.11.1.min
 //= require jquery.mobile-1.4.5.min
 //= require flash
